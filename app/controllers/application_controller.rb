150eda7d86abc30c47b580c9dd90f7d24034b26a
JoA Azria <joa.azria@gmail.com>
2020-12-12 19:47:09 -0300

    cloudinary + device com avatar e username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+   before_action :authenticate_user!, except: [:show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :username])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username])
+
+
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :companies
+  has_many :ejobers
+  validates :username, presence: true
+
+   has_one_attached :avatar
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
