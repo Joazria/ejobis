58dfd0defea51abc9076d3924e5a42a24448aa28
JoA Azria <joa.azria@gmail.com>
2020-12-13 10:26:50 -0300

    generate device views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :companies
   has_many :ejobers
-  validates :username, presence: true
+  validates_uniqueness_of :username
 
-   has_one_attached :avatar
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  get '/profile', to: 'pages#profile'
+  get '/users', to: 'pages#profile'
+ # get '/profile', to: 'pages#profile', as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :companies do
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   end
   resources :ejobis, only: [:destroy, :update, :edit]
   resources :ejobers
+
 end
