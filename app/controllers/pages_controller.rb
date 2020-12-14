011a8d28dfd04cd64e130a28afd4c6f2b3982b5d
JoA Azria <joa.azria@gmail.com>
2020-12-14 14:31:09 -0300

    front end home page + organização perfil ejober

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+
   def home
+
+    if params[:query].present?
+      @ejobers = Ejober.search_by_name(params[:query])
+    else
+      @ejobers = Ejober.all
+    end
   end
 
-
   def profile
+
   end
 end
